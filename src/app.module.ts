1d6b40ad9b201b8c9b3a32e59624011aebf66966
kolynzb <collinsbenda360@gmail.com>
2023-03-10 01:44:30 +0300

    chp14

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { Product } from './models/product.entity';
+import { ProductsService } from './models/products.service';
 import { ProductsController } from './product.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Product])],
   controllers: [AppController, ProductsController],
-  providers: [],
+  providers: [ProductsService],
 })
 export class AppModule {}
